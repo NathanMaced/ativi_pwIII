4a3dff01fa6646dc3f455797a7fde1e2eba05c00
NathanMaced <nathanmacedo02@gmail.com>
2023-03-29 22:04:23 -0300

    Começando

--- empregado.php
+++ empregado.php
@@ -2,19 +2,24 @@
 
 class Funcionarios{
     var $codigo;
-    var $nome;
-    var $sobrenome;
+    String $nome;
+    String $sobrenome;
+    String $cpf
     var $funcao;
     var $admissao;
     var $salario;
 
-    function __construct($codigo, $nome, $sobrenome, $funcao, $admissao, $salario){
+    function __construct($codigo, $nome, $sobrenome,$cpf, $funcao, $admissao, $salario){
         $this->codigo=$codigo;
         $this->nome=$nome;
         $this->sobrenome=$sobrenome;
+        $this->cpf=$cpf;
         $this->funcao=$funcao;
         $this->admissao=$admissao;
         $this->salario=$salario;
+    }
+    function __destruct(){
+        echo "<br>Objeto {$this->nome} finalizando ... <br>";
     }
 }
 
