babdf148d64954fc4ff20eb634842a5d7a7404af
NathanMaced <nathanmacedo02@gmail.com>
2023-04-03 22:07:54 -0300

    Continuação

--- assalariado.php
+++ assalariado.php
@@ -3,6 +3,13 @@
 class Assalariados{
     var $salario;
     var $inss;
+    var $vt;
+
+    function desconto($salario){
+        if($salario>1200){
+            this->salario = $salario / 0.06%
+}
+}
 }
 
 ?>
--- empregado.php
+++ empregado.php
@@ -1,26 +1,9 @@
 <?php
 
-class Funcionarios{
-    var $codigo;
-    String $nome;
-    String $sobrenome;
-    String $cpf
-    var $funcao;
-    var $admissao;
-    var $salario;
 
-    function __construct($codigo, $nome, $sobrenome,$cpf, $funcao, $admissao, $salario){
-        $this->codigo=$codigo;
-        $this->nome=$nome;
-        $this->sobrenome=$sobrenome;
-        $this->cpf=$cpf;
-        $this->funcao=$funcao;
-        $this->admissao=$admissao;
-        $this->salario=$salario;
-    }
-    function __destruct(){
-        echo "<br>Objeto {$this->nome} finalizando ... <br>";
-    }
-}
+include_once 'assalariado.php';
+
+
+echo "{$salario->desconto}";
 
 ?>
