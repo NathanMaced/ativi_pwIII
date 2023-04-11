b3cef9f406ba6bd689e2a8daa780a63174d800ba
NathanMaced <nathanmacedo02@gmail.com>
2023-04-10 21:46:11 -0300

    Criando function

--- assalariado.php
+++ assalariado.php
@@ -1,10 +1,22 @@
 <?php
 
+include_once 'funcionario.php';
+
 class Assalariados{
     var $salario;
     var $inss;
     var $vt;
+    var $salarioLiquido;
 
+}
+
+function inss ($salario){
+    if($salario>1300){
+        $salarioLiquido = $salario - 0.06%;
+        
+    }
+    
+    return $salarioLiquido;
 }
 
 
